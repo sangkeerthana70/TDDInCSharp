30f2419232cd5f3b81f1a12fa9e5f43bbca99238
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-05 14:01:01 -0500

    Arrays:Left Rotation in TDD

---
+++ ArraysLeftRotateTDD/LeftRotateArray.UnitTests/RotateArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace LeftRotateArray.UnitTests
+{
+    public class RotateArray
+    {
+        public static int GiveNewRotatedArray(int[] inputArr)
+        {
+            
+                return 5;
+
+        }
+
+
+    
+
+        public static int GiveNewRotatedArray(int[] inputArr, int numToRotate)
+        {
+            //int[] newArray = new int[] { };
+
+            for(int i = 0; i <= inputArr.Length; i++)
+            {
+                inputArr[i - 1] = inputArr[i];
+                
+            }
+            //newArray[inputArr.Length]
+            var newArray = inputArr[inputArr.Length - 1];
+            Console.WriteLine(newArray);
+            return newArray;
+            
+        }
+    }
+}
---
+++ ArraysLeftRotateTDD/LeftRotateArray.UnitTests/RotateArrayTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeftRotateArray.UnitTests
+{
+    [TestFixture]
+    public class RotateArrayTests
+    {
+        [Test]
+        public void CheckLengthOfArr()
+        {
+            int[] inputArr = new int[] { 1, 2, 3, 4, 5 };
+            int arrLength = 5;
+            //int numToRotate = 4;
+
+            var expectedResult = 5;
+            //var rotateArray = new RotateArray();
+            var actualResult = RotateArray.GiveNewRotatedArray(inputArr);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        
+        [Test]
+        public void RotateArrayBasedOnNumToRotate()
+        {
+            int[] inputArr = new int[] { 1, 2, 3, 4, 5 };
+            int arrLength = 5;
+            int numToRotate = 4;
+
+            var expectedResult = new int[] { 2, 3, 4, 5, 1 };
+            var actualResult = RotateArray.GiveNewRotatedArray(inputArr, numToRotate);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
