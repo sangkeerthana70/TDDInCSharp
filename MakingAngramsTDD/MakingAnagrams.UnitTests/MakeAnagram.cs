51ec22d751109a8386f57b57d00a28cf393373bf
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-05 22:49:12 -0500

    Solve make anagram challenge using TDD

---
+++ MakingAngramsTDD/MakingAnagrams.UnitTests/MakeAnagram.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakingAnagrams.UnitTests
+{
+    class MakeAnagram
+    {
+        internal int FindLetters(string a, string b)
+        {
+            //test case2
+            if (a.Length == b.Length)
+            {
+                Console.WriteLine("Length is equal");
+            }
+
+
+           //test case3
+           //Compare string a with string b
+            int mismatchCount = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine("outer loop: " + a[i]);
+                bool match = false;
+                for (int j = 0; j < b.Length; j++)
+                {
+                    Console.WriteLine("Inner loop: " + b[j]);
+                    if(a[i] == b[j])
+                    {
+                        Console.WriteLine("match");
+                        match = true;
+                        break;
+                    }
+                } 
+                if (!match)
+                {
+                    Console.WriteLine("no match");
+                    mismatchCount++;
+                    Console.WriteLine("mismatchCount " + mismatchCount);
+                }
+            }
+            //compare string b with string a
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.WriteLine("outer loop: " + b[i]);
+                bool match = false;
+                for (int j = 0; j < a.Length; j++)
+                {
+                    Console.WriteLine("Inner loop: " + a[j]);
+                    if (b[i] == a[j])
+                    {
+                        Console.WriteLine("match");
+                        match = true;
+                        break;
+                    }
+                }
+                if (!match)
+                {
+                    Console.WriteLine("no match");
+                    mismatchCount++;
+                    Console.WriteLine("mismatchCount " + mismatchCount);
+                }
+            }
+            Console.WriteLine("Mis match Letter Count: " + mismatchCount);
+            return mismatchCount;
+
+           
+        }
+
+    }
+}
---
+++ MakingAngramsTDD/MakingAnagrams.UnitTests/MakingAnagramTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakingAnagrams.UnitTests
+{
+    class MakingAnagramTests
+    {
+        [Test]
+        public void ReturnsAnInteger()
+        {
+            string a = "";
+            string b = "";
+            var makeAnagram = new MakeAnagram();
+            var expectedResult = 0;
+
+            int actualResult = makeAnagram.FindLetters(a, b);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void CheckIfBothStringHaveSameLength()
+        {
+            string a = "cde";
+            string b = "dcf";
+            var makeAnagram = new MakeAnagram();
+            var expectedResult = true;
+
+            int actualResult = makeAnagram.FindLetters(a, b);
+            bool boolValue = actualResult != 0;
+            Assert.AreEqual(expectedResult, !boolValue);
+        }
+
+        [Test]
+        public void ReturnFourforThisInput()
+        {
+
+            string a = "cde";
+            string b = "abc";
+            var makeAnagram = new MakeAnagram();
+            var expectedResult = 4;
+
+            int actualResult = makeAnagram.FindLetters(a, b);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+
+    }
+}
