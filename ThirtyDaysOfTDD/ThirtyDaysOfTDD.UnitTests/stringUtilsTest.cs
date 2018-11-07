71d151be5785b18165ede6bcfd6e647c8b75707b
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-07 16:05:33 -0500

    Day three of 30DaysTdd

---
+++ ThirtyDaysOfTDD/ThirtyDaysOfTDD.UnitTests/stringUtilsTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ThirtyDaysOfTDD.UnitTests
+{
+
+    [TestFixture]
+
+    public class StringUtilsTest
+    {
+        [Test]
+        public void ShouldBeAbleToCountNumberOfLettersInSimpleSentence()
+        {
+            var sentenceToScan = "TDD is awesome!";
+            var characterToScanFor = "e";
+            var expectedResult = 2;
+
+            var stringUtilsTest = new StringUtilsTest();
+
+            int result = FindNumberOfOccurances(sentenceToScan, characterToScanFor);
+        }
+    }
+}
