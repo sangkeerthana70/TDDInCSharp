4a0ae12c4f465e7aea21f76b2640ff610c28b2d4
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-06 20:10:29 -0500

    Setting up environment for Nunit in C#

---
+++ unit-testing-using-nunit/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using Prime.Service;
+
+namespace Prime.UnitTests.Services
+{
+    [TestFixture]
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Test]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.IsFalse(result, "1 should not be prime");
+        }
+
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(1)]
+        public void ReturnFalseGivenValuesLessThan2(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.IsFalse(result, $"{value} should not be prime");
+        }
+    }
+}
---
+++ unit-testing-using-nunit/PrimeService/PrimeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Prime.Service
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if(candidate < 2)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Please create a test first");
+        }
+    }
+}
