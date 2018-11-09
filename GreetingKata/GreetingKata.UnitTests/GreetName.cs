b6dfbb77e27d0e3b40cad86cb7e5b32f02648a1b
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-09 13:52:03 -0500

    Greeting kata TDD initial commit

---
+++ GreetingKata/GreetingKata.UnitTests/GreetName.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingKata.UnitTests
+{
+    internal class GreetName
+    {
+        public string ReturnGreetName(string name)
+        {
+            if(name == "")
+            {
+                return ("Hello, my friend.");
+            }
+
+            if(name == name.ToUpper())
+            {
+                return ("HELLO " + name.ToUpper() + "!");
+            }
+
+            return ("Hello" + ", " + name + ".");
+
+        }
+
+        public string ReturnGreetName(string[] names)
+        {
+            var newString = "";
+            var strArrLength = names.Length;
+            Console.WriteLine(strArrLength);
+            for (int i = 0; i < strArrLength; i++)
+            {
+                if (strArrLength == 2)
+                {
+                    newString = ("Hello, " + names[i]);
+                    i++;
+                }
+                newString += (" and " + names[i] + ".");
+               
+            }
+            return newString;
+            
+        }
+
+    }
+}
---
+++ GreetingKata/GreetingKata.UnitTests/GreetingKataTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingKata.UnitTests
+{
+    public class GreetingkataTests
+    {
+        [Test]
+        public void GreetNameShouldReturnHelloBob()
+        {
+            string name = "";
+            var greetName = new GreetName();
+
+            var expectedResult = greetName.ReturnGreetName("Bob");
+            var actualResult = "Hello, Bob.";
+            Console.WriteLine(expectedResult);
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [Test]
+        public void GreetNameShouldHandleNull()
+        {
+            string name = "";
+            var greetName = new GreetName();
+
+            var expectedResult = greetName.ReturnGreetName("");
+            var actualResult = "Hello, my friend.";
+            Console.WriteLine(expectedResult);
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [Test]
+        public void GreetNameShouldHandleUppercaseString()
+        {
+            string name = "JERRY";
+            var greetName = new GreetName();
+
+            var expectedResult = greetName.ReturnGreetName("JERRY");
+            var actualResult = "HELLO JERRY!";
+            Console.WriteLine(expectedResult);
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [Test]
+        public void GreetNameShouldHandleTwoNamesOfInput()
+        {
+            string[] names = new string[] { "Jill", "Jane" };
+            var greetName = new GreetName();
+
+            var expectedResult = greetName.ReturnGreetName(names);
+            var actualResult = "Hello, Jill and Jane.";
+            Console.WriteLine(expectedResult);
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [Test]
+        public void GreetNameShouldHandleMoreThanTwoNamesOfInput()
+        {
+            string[] names = new string[] { "Amy", "BRIAN", "Charlotte" };
+            var greetName = new GreetName();
+
+            var expectedResult = greetName.ReturnGreetName(names);
+            var actualResult = "Hello, Amy, Brian, and Charlotte.";
+            Console.WriteLine(expectedResult);
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+    }
+}
