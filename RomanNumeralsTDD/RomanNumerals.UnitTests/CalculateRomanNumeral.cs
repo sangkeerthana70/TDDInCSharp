985aee382d1f771fb32aae53a603f15120ce3330
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-09 14:01:53 -0500

    Convert RomanNumeral to decimal in TDD approach

---
+++ RomanNumeralsTDD/RomanNumerals.UnitTests/CalculateRomanNumeral.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace RomanNumerals.UnitTests
+{
+    internal class CalculateRomanNumeral
+    {
+        
+        public int FindRomanNumeral(char[] inputArr)
+        {
+            //int result = 0;
+            int tempResult = 0;
+            string result = "";
+
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                tempResult = RomanToNumeric(inputArr[i]);
+                result += tempResult.ToString();
+                
+                
+            }
+            
+            try
+            {
+                Console.WriteLine("In try block");
+                Console.WriteLine("final conversion: " + Int32.Parse(result));
+                return Int32.Parse(result);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("In exception block");
+                return 0;
+            }
+            
+            
+        }
+
+        private int RomanToNumeric(char r)
+        {
+            var num = 0;
+            switch (r)
+            {
+                case 'I':
+                    num = 1;
+                    Console.WriteLine("Current val of i: " + 1);
+                    break;
+                case 'V':
+                    num = 5;
+                    break;
+                case 'X':
+                    num = 10;
+                    break;
+                case 'L':
+                    num = 50;
+                    break;
+                case 'C':
+                    num = 100;
+                    break;
+                case 'D':
+                    num = 500;
+                    break;
+                case 'M':
+                    num = 1000;
+                    break;
+                
+            }
+            return num;
+        }
+    }
+}
---
+++ RomanNumeralsTDD/RomanNumerals.UnitTests/TDDApproach.cs
@@ -0,0 +1,115 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals.UnitTests
+{
+    public class TDDApproach
+    {
+        
+        [Test]
+        public void ReturnEmptyCharArr()
+        {
+            char[] inputArr = new char[0] { };
+            var expected = 0;
+
+            var calculateRomanNumeral = new CalculateRomanNumeral();
+
+            var actualResult = calculateRomanNumeral.FindRomanNumeral(inputArr);
+            Console.WriteLine("In Test");
+            Assert.AreEqual(expected, actualResult);
+            
+        }
+
+        [Test]
+        public void ReturnValue1ForCapitalI()
+        {
+            char[] inputArr = new char[] { 'I' };
+            var expected = 1;
+
+            var calculateRomanNumeral = new CalculateRomanNumeral();
+
+            var actualResult = calculateRomanNumeral.FindRomanNumeral(inputArr);
+            Console.WriteLine(actualResult);
+            Assert.AreEqual(expected, actualResult);
+        }
+        [Test]
+        public void ReturnValue10ForCapitalX()
+        {
+            char[] inputArr = new char[] { 'X' };
+            var expected = 10;
+
+            var calculateRomanNumeral = new CalculateRomanNumeral();
+
+            var actualResult = calculateRomanNumeral.FindRomanNumeral(inputArr);
+            Console.WriteLine(actualResult);
+            Assert.AreEqual(expected, actualResult);
+        }
+        [Test]
+        public void ReturnValue50ForCapitalL()
+        {
+            char[] inputArr = new char[] { 'L' };
+            var expected = 50;
+
+            var calculateRomanNumeral = new CalculateRomanNumeral();
+
+            var actualResult = calculateRomanNumeral.FindRomanNumeral(inputArr);
+            Console.WriteLine(actualResult);
+            Assert.AreEqual(expected, actualResult);
+        }
+        [Test]
+        public void ReturnValue100ForCapitalC()
+        {
+            char[] inputArr = new char[] { 'C' };
+            var expected = 100;
+
+            var calculateRomanNumeral = new CalculateRomanNumeral();
+
+            var actualResult = calculateRomanNumeral.FindRomanNumeral(inputArr);
+            Console.WriteLine(actualResult);
+            Assert.AreEqual(expected, actualResult);
+        }
+
+        [Test]
+        public void ReturnValue500ForCapitalD()
+        {
+            char[] inputArr = new char[] { 'D' };
+            var expected = 500;
+
+            var calculateRomanNumeral = new CalculateRomanNumeral();
+
+            var actualResult = calculateRomanNumeral.FindRomanNumeral(inputArr);
+            Console.WriteLine(actualResult);
+            Assert.AreEqual(expected, actualResult);
+        }
+
+        [Test]
+        public void ReturnValue1000ForCapitalM()
+        {
+            char[] inputArr = new char[] { 'M' };
+            var expected = 1000;
+
+            var calculateRomanNumeral = new CalculateRomanNumeral();
+
+            var actualResult = calculateRomanNumeral.FindRomanNumeral(inputArr);
+            Console.WriteLine(actualResult);
+            Assert.AreEqual(expected, actualResult);
+        }
+
+        [Test]
+        public void ReturnTwoValuesForTwoStrings()
+        {
+            char[] inputArr = new char[] { 'I', 'I' };
+            var expected = 11;
+
+            var calculateRomanNumeral = new CalculateRomanNumeral();
+
+            var actualResult = calculateRomanNumeral.FindRomanNumeral(inputArr);
+            Console.WriteLine(actualResult);
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}
