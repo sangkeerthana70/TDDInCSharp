ead5b45472c47a7aff3738a3350a42696f874fdc
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-10 21:17:11 -0500

    Meet requirements 1 through 4

---
+++ StringCalculatorKata/StringCalculator.UnitTests/AddString.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.UnitTests
+{
+    class AddString
+    {
+        public int Add(string numbers)
+        {
+            int sum = 0;
+            char delimiter = ',';
+
+            bool negativeValues = false;
+            string negativeNumber = "";
+            if (numbers == "")
+            {
+                return 0;
+            }
+
+            if (numbers.Length == 1)
+            {
+                return Int32.Parse(numbers);
+            }
+
+            if (numbers.Substring(0, 2) == "//")
+            {
+                delimiter = Convert.ToChar(numbers.Substring(2, 1));
+                Console.WriteLine("Delimiter : " + delimiter);
+                // slice first four chars in "//;\n1;2"
+                numbers = numbers.Substring(4);
+            }
+
+
+            var strArr = numbers.Split(delimiter);
+            List<string> strList = new List<string>();
+
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                //Console.WriteLine("After slicing 4 chars: " + strArr[i]);
+                
+                strList.AddRange(strArr[i].Split('\n'));
+
+                
+            }
+
+            foreach(string num in strList)
+            {
+                if (Int32.Parse(num) < 0)
+                {
+                    negativeValues = true;
+                    negativeNumber = negativeNumber + num;
+                    Console.WriteLine("Negative value " + negativeNumber);
+                }
+                Console.WriteLine("num = " + num);
+                sum += Int32.Parse(num.Trim());
+                Console.WriteLine("Sum = " + sum);
+            }
+            if (negativeValues)
+            {
+                throw new System.ArgumentException("negatives not allowed");
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ StringCalculatorKata/StringCalculator.UnitTests/TDDApproach.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.UnitTests
+{
+    [TestFixture]
+    public class TDDApproach
+    {
+        [Test]
+        public void EmptyStringShouldReturn0()
+        {
+            string inputString = "";
+
+            var addString = new AddString();
+            var expectedResult = 0;
+            var actualResult = addString.Add(inputString);
+
+            Console.WriteLine(actualResult);
+            Assert.AreEqual(actualResult, expectedResult);           
+        }
+
+        [Test]
+        public void StringInputOfOneNumberShouldReturnNumber()
+        {
+            string inputString = "1";
+
+            var addString = new AddString();
+            var expectedResult = 1;
+            var actualResult = addString.Add(inputString);
+
+            Console.WriteLine(actualResult);
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [Test]
+        public void StringInputOfTwoNumberShouldReturnSumOfTwoNumbers()
+        {
+            string inputString = "1,2";
+
+            var addString = new AddString();
+            var expectedResult = 3;
+            var actualResult = addString.Add(inputString);
+
+            Console.WriteLine(actualResult);
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [Test]
+        public void StringInputOfManyNumbersShouldReturnSumOfAllNumbers()
+        {
+            string inputString = "1, 2, 3, 4, 5";
+
+            var addString = new AddString();
+            var expectedResult = 15;
+            var actualResult = addString.Add(inputString);
+
+            Console.WriteLine(actualResult);
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [Test]
+        public void StringInputShouldHandleNewlinesBetweenNumbersInsteadOfCommas()
+        {
+            string inputString = "1\n2,3";
+
+            var addString = new AddString();
+            var expectedResult = 6;
+            var actualResult = addString.Add(inputString);
+
+            Console.WriteLine(actualResult);
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [Test]
+        public void StringInputShouldSupportDifferentDelimiters()
+        {
+            string inputString = "//;\n1;2";
+
+            var addString = new AddString();
+            var expectedResult = 3;
+            var actualResult = addString.Add(inputString);
+
+            Console.WriteLine(actualResult);
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [Test]
+        public void StringInputWithNegativeValuesShouldThrowException()
+        {
+            string inputString = "1, -1, 0";
+
+            var addString = new AddString();
+            var expectedResult = "negatives not allowed";
+            var actualResult = addString.Add(inputString);
+
+            Console.WriteLine(actualResult);
+            Assert.AreEqual(actualResult, expectedResult);
+
+        }
+
+
+    }
+}
