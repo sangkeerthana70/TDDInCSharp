a29356aaadf2b89bcf7538091fbc1a2406a4aa98
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-09 23:20:33 -0500

    First Unit test to compare string length implemented

---
+++ CompareTwoStringsForCharDiff/CompareStrings/StringCompare.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareStrings
+{
+    public class CompareTwoStrings
+    {
+        public int CheckLengthOfTwoStrings(string a, string b)
+        {
+            int strLength = 0;
+            for(int i = 0; i < a.Length; i++)
+            {
+                if (a.Length == b.Length)
+                {
+                    strLength = a.Length;
+                }
+                else
+                {
+                    throw new System.ArgumentException("Array length is not equal");
+                }
+
+            }
+            return strLength;
+        }
+        
+    }
+}
