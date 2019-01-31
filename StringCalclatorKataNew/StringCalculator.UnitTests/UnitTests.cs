eadc73d18a953ca25c24e03421a4bc4a208806a4
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-31 16:21:32 -0500

    Revisiting String calculator Kata

---
+++ StringCalclatorKataNew/StringCalculator.UnitTests/UnitTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using StringCalculatorService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorUnitTests
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        //Assert.IsInstanceOf()
+        [Test]
+        public void InputOfEmptyStringShouldReturnZero()
+        {
+            var input = "";
+            var expected = 0;
+            var actual = StringCalculator.Add(input);
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+
+
+        }
+        [Test]
+        public void InputOneNumberShouldReturnTheNumber()
+        {
+            var input = "5";
+            var expected = 5;
+            var actual = StringCalculator.Add(input);
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public void InputTwoNumbersShouldReturnTheSumOfTwoNumbers()
+        {
+            var input = "1,2";
+            var expected = 3;
+            var actual = StringCalculator.Add(input);
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public void InputOfMoreNumbersShouldReturnTheSumOfAllNumbers()
+        {
+            var input = "1,2,3,4,5";
+            var expected = 15;
+            var actual = StringCalculator.Add(input);
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
---
+++ StringCalclatorKataNew/StringCalculatorService/StringCalculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorService
+{
+    public class StringCalculator
+    {
+        
+
+        
+
+        public static int Add(string numbers)
+        {
+            int sum = 0;
+            if (numbers == "")
+            {
+                return sum;
+            }
+            if(numbers.Length == 1)
+            {
+                sum = Convert.ToInt32(numbers);
+            }
+            if(numbers.Length > 1)
+            {
+
+                //stringnum = "10,20,30,40";
+                string[] val = numbers.Split(',');
+                
+                for (int i = 0; i < val.Length; i++)
+                {
+                    sum += int.Parse(val[i]);
+
+
+                    //sum = Convert.ToInt32(numbers);
+                    Console.WriteLine(sum);
+
+                }
+
+            }
+            return sum;
+
+
+        }
+    }
+}
